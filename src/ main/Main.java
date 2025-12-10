7a9e522684e202299f70229272846a2dd5909295
Nikshitha20 <addagantinikshitha@outlook.com>
2025-12-10 18:14:43 +0530

    Implement main method to add and retrieve student

--- src/ main/Main.java
+++ src/ main/Main.java
@@ -1,1 +1,17 @@
+package main;
+
+import model.Student;
+import dao.StudentDAO;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student student = new Student(101, "Nikshitha", "CSE", 85);
+
+        StudentDAO dao = new StudentDAO();
+        dao.addStudent(student);
+        dao.getStudent(101);
+    }
+}
 
