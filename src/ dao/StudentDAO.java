ee77783c4c73cd0bb62e074d846a0a6b6eeac33a
Nikshitha20 <addagantinikshitha@outlook.com>
2025-12-10 18:14:17 +0530

    Add StudentDAO with add and get methods
    Implemented methods to add and fetch student details.

--- src/ dao/StudentDAO.java
+++ src/ dao/StudentDAO.java
@@ -1,1 +1,21 @@
+package dao;
+
+import model.Student;
+
+public class StudentDAO {
+
+    // Simulated database operation
+    public void addStudent(Student student) {
+        System.out.println("Student added successfully:");
+        System.out.println("ID: " + student.getStudentId());
+        System.out.println("Name: " + student.getName());
+        System.out.println("Department: " + student.getDepartment());
+        System.out.println("Marks: " + student.getMarks());
+    }
+
+    public void getStudent(int id) {
+        System.out.println("Fetching student details for ID: " + id);
+        // Database logic would be implemented here
+    }
+}
 
