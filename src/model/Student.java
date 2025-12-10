a427b90ac81c00723568418992d7d5001e1fb163
Nikshitha20 <addagantinikshitha@outlook.com>
2025-12-10 18:13:42 +0530

    Add Student class with attributes and constructor

--- src/model/Student.java
+++ src/model/Student.java
@@ -1,1 +1,33 @@
+package model;
+
+public class Student {
+
+    private int studentId;
+    private String name;
+    private String department;
+    private int marks;
+
+    public Student(int studentId, String name, String department, int marks) {
+        this.studentId = studentId;
+        this.name = name;
+        this.department = department;
+        this.marks = marks;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+}
 
